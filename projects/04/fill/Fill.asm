947084d47e561ac15f813297f383ac09e73ea127
Drea Esposito <dreaesposito@hotmail.com>
2023-08-04 23:10:59 -0500

    completed Fill.asm in project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,55 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+
 // Put your code here.
+
+// pseudocode (infinite loop)
+//if (keyIsPressed) 
+// loop through each pixel in the screen and fill black
+//else 
+//loop through each pixel in the screen and fill white
+
+(BEGIN)  	
+	@8192 // each row (256 total) of the screen consists of 32 consecutive 16-bit words
+	D=A     
+	@i                   
+	M=D // intialize var i to start at pixel 8192 (32 * 256)
+
+(LOOP)      
+	@i
+	M=M-1 // i--
+	D=M
+	@BEGIN
+	D;JLT // if (i < 0), then reset i back to 8192                 
+	
+    @KBD // load keyboard into memory     
+	D=M
+	
+    @DRAWWHITE 	        
+	D;JEQ // if (RAM[KBD] == 0), then do not fill (no key is being pressed)
+	
+    @DRAWBLACK // else, fill the screen (a key is being pressed)
+	0;JMP 
+
+(DRAWBLACK)             
+	@SCREEN // predefined symbol for the first pixel in the screen memory map   
+	D=A // load into memory 
+	
+    @i
+	A=D+M              
+	M=-1           
+	
+    @LOOP             
+	0;JMP
+
+(DRAWWHITE)
+	@SCREEN // predefined symbol for the first pixel in the screen memory map             
+	D=A // load into memory                
+	
+    @i        
+	A=D+M              
+	M=0                
+	
+    @LOOP           
+	0;JMP
